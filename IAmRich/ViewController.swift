bc183b9970b2b794946630ec47f5660c83842dab
jayun-o <beam.wannasiri@gmail.com>
2024-05-27 19:14:07 +0700

    Initial Commit

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by typhoon on 27/5/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
